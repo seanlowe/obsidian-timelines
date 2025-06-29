cf6c0cd6ae6cb5230e1d27e6918a7c528f689954
Sean Lowe <35589586+seanlowe@users.noreply.github.com>
2025-06-28 22:12:35 -0600

    render datelabels smaller on collapsed events

--- src/react/timeline-header.tsx
+++ src/react/timeline-header.tsx
@@ -2,5 +2,9 @@ import { FC } from 'react'
 import { TimelineHeaderProps } from '../types'
 
 export const TimelineHeader: FC<TimelineHeaderProps> = ({ date, side }) => {
+  if (date.includes('to')) {
+    return <h3 style={{ textAlign: side }}>{date}</h3>
+  }
+
   return <h2 style={{ textAlign: side }}>{date}</h2>
 }
